048e690d58355299e207219914fd5c8c28eaf396
daisy219 <daisyzhao219@outlook.com>
2020-11-04 17:12:19 +0800

    完善播放器

--- src/application/Player/index.tsx
+++ src/application/Player/index.tsx
@@ -13,83 +13,11 @@ import {
 } from './store/actionCreators';
 import MiniPlayer from './miniPlayer';
 import NormalPlayer from './normalPlayer';
+import Toast from '@/components/toast/index';
 import { shuffle, findIndex } from '@/api/utils';
 import { isEmptyObject } from '@/utils/index';
-// const playList = [
-//   {
-//     ftype: 0,
-//     djId: 0,
-//     a: null,
-//     cd: '01',
-//     crbt: null,
-//     no: 1,
-//     st: 0,
-//     rt: '',
-//     cf: '',
-//     alia: [
-//       '手游《梦幻花园》苏州园林版推广曲'
-//     ],
-//     rtUrls: [],
-//     fee: 0,
-//     s_id: 0,
-//     copyright: 0,
-//     h: {
-//       br: 320000,
-//       fid: 0,
-//       size: 9400365,
-//       vd: -45814
-//     },
-//     mv: 0,
-//     al: {
-//       id: 84991301,
-//       name: '拾梦纪',
-//       picUrl: 'http://p1.music.126.net/M19SOoRMkcHmJvmGflXjXQ==/109951164627180052.jpg',
-//       tns: [],
-//       pic_str: '109951164627180052',
-//       pic: 109951164627180050
-//     },
-//     name: '拾梦纪',
-//     l: {
-//       br: 128000,
-//       fid: 0,
-//       size: 3760173,
-//       vd: -41672
-//     },
-//     rtype: 0,
-//     m: {
-//       br: 192000,
-//       fid: 0,
-//       size: 5640237,
-//       vd: -43277
-//     },
-//     cp: 1416668,
-//     mark: 0,
-//     rtUrl: null,
-//     mst: 9,
-//     dt: 234947,
-//     ar: [
-//       {
-//         id: 12084589,
-//         name: '妖扬',
-//         tns: [],
-//         alias: []
-//       },
-//       {
-//         id: 12578371,
-//         name: '金天',
-//         tns: [],
-//         alias: []
-//       }
-//     ],
-//     pop: 5,
-//     pst: 0,
-//     t: 0,
-//     v: 3,
-//     id: 1416767593,
-//     publishTime: 0,
-//     rurl: null
-//   }
-// ];
+import { playMode } from '@/api/config';
+
 const Player = React.forwardRef((props: any, ref: any) => {
   const audioRef = useRef() as any;
   const {
@@ -117,6 +45,9 @@ const Player = React.forwardRef((props: any, ref: any) => {
   const [duration, setDuration] = useState(0);
   // 记录当前的歌曲。以便于下次重渲染时比对是否是一首歌
   const [preSong, setPreSong] = useState({} as any);
+  // 模式文字
+  const [modeText, setModeText] = useState('');
+  const toastRef = useRef() as any;
 
   useEffect(() => {
     changeCurrentIndexDispatch(0);
@@ -138,17 +69,21 @@ const Player = React.forwardRef((props: any, ref: any) => {
       changePlayListDispatch(sequencePlayList);
       let index = findIndex(currentSong, sequencePlayList);
       changeCurrentIndexDispatch(index);
+      setModeText('顺序循环');
     } else if (newMode === 1) {
       // 单曲循环
       changePlayListDispatch(sequencePlayList)
+      setModeText('单曲循环');
     } else if (newMode === 2) {
       // 随机播放
       let newList = shuffle(sequencePlayList);
       let index = findIndex(currentSong, newList);
       changePlayListDispatch(newList);
       changeCurrentIndexDispatch(index);
+      setModeText('随机播放');
     }
     changeModeDispatch(newMode);
+    toastRef.current.show();
   };
 
   // 获取播放列表
@@ -227,6 +162,13 @@ const Player = React.forwardRef((props: any, ref: any) => {
     }
     changeCurrentIndexDispatch(index);
   }
+  const handleEnd = () => {
+    if (mode === playMode.loop) {
+      handleLoop();
+    } else {
+      handleNext();
+    }
+  }
   return (
     <div>
       {
@@ -261,7 +203,9 @@ const Player = React.forwardRef((props: any, ref: any) => {
       <audio
         ref={audioRef}
         onTimeUpdate={updateTime}
+        onEnded={handleEnd}
       ></audio>
+      <Toast text={modeText} ref={toastRef}></Toast>
     </div>
   )
 })
